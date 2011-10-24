184f6f46483981976f3dd1cf126560d49e078fcc
Justin Adler <github@world-domination.com.au>
2011-10-24 19:26:22 +1100

    - Added RavenDb Embedded v499. - Added NUnit. - Added Testing project.

---
+++ Code/RavenOverflow.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Raven.Client.Embedded;
+
+namespace RavenOverflow.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Test]
+        public void GivenSomeQuestions_Index_ReturnsTheMostRecentQuestions()
+        {
+            
+        }
+    }
+}
